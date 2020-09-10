87c321c1477b1f35f0f3a4f90398ec39b5c1c656
Mayank Agarwal <mayankgrwl97@gmail.com>
2020-09-10 20:23:06 +0530

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="human-parser",
+    version="1.0",
+    author="Mayank Agarwal",
+    author_email="mayankgrwl97@gmail.com",
+    description="Human Part Segmentation",
+    packages=setuptools.find_packages(),
+)
