bef814773fe77738dcd250e2f546cb4f7b56cc76
Mayank Agarwal <mayankgrwl97@gmail.com>
2020-09-13 12:47:29 +0530

    Update setup.py to install C++/Cuda/header files at site-packages location.
    Refer blog for instructions --> https://kiwidamien.github.io/making-a-python-package-vi-including-data-files.html

--- setup.py
+++ setup.py
@@ -7,4 +7,8 @@ setuptools.setup(
     author_email="mayankgrwl97@gmail.com",
     description="Human Part Segmentation",
     packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={'': ['modules/src/*.h', 'modules/src/*.cpp',
+                       'modules/src/*.cu', 'modules/src/utils/*.h',
+                       'modules/src/utils/*.cuh']}
 )
